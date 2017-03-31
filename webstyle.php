619513790d0f0e53cb7a8179abae4e60e74c1d52
marieta@workabroad.ph <marieta@workabroad.ph>
2017-03-31 14:33:02 +0800

    hai mayeth

--- webstyle.php
+++ webstyle.php
@@ -3,3 +3,7 @@
 <script type="text/javascript" language="javascript1.2" src="../styles/webcustom.js"></script>
 <script type="text/javascript" language="javascript1.2" src="../styles/style.js"></script>
 <link rel="stylesheet" type="text/css" href="../styles/style.css">
+
+<?php
+echo "Mayeth";
+?>
